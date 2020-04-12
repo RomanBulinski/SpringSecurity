6cc69da5998c338bad2fb4ab65d5c75809438153
roman <aboandrus@gmail.com>
2020-04-12 07:51:34 +0200

    Start app

---
+++ src/main/java/rom/buulean/security/SecurityHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package rom.buulean.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityHelloWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rom/buulean/security/SecurityHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package rom.buulean.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
